ce5cb1f3783bfe8ef6a65461051f7c452d747bf4
Nujitha Wickramasurendra <nwickramasurendra@gmail.com>
2020-03-21 18:40:33 +0530

    reading from file, creating graph with JgraphT implemented

--- src/main/java/FordFulkerson/Graph.java
+++ src/main/java/FordFulkerson/Graph.java
@@ -1,5 +1,8 @@
 package FordFulkerson;
 
+/*
+package FordFulkerson;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -58,4 +61,69 @@ public class Graph {
             System.out.println();
         }
     }
+}
+*/
+
+
+import org.jgrapht.Graphs;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.*;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Graph {
+    public static void main(String args[]) {
+
+        SimpleDirectedWeightedGraph<String, DefaultWeightedEdge> graph =
+                new SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>
+                        (DefaultWeightedEdge.class);
+
+        graph.addVertex("vertex1");
+        graph.addVertex("vertex2");
+        graph.addVertex("vertex3");
+        graph.addVertex("vertex4");
+        graph.addVertex("vertex5");
+
+
+        DefaultWeightedEdge e1 = graph.addEdge("vertex1", "vertex2");
+        graph.setEdgeWeight(e1, 5);
+
+        DefaultWeightedEdge e2 = graph.addEdge("vertex2", "vertex3");
+        graph.setEdgeWeight(e2, 3);
+
+        DefaultWeightedEdge e3 = graph.addEdge("vertex4", "vertex5");
+        graph.setEdgeWeight(e3, 6);
+
+        DefaultWeightedEdge e4 = graph.addEdge("vertex2", "vertex4");
+        graph.setEdgeWeight(e4, 2);
+
+        DefaultWeightedEdge e5 = graph.addEdge("vertex5", "vertex4");
+        graph.setEdgeWeight(e5, 4);
+
+        DefaultWeightedEdge e6 = graph.addEdge("vertex2", "vertex5");
+        graph.setEdgeWeight(e6, 9);
+
+        DefaultWeightedEdge e7 = graph.addEdge("vertex4", "vertex1");
+        graph.setEdgeWeight(e7, 7);
+
+        DefaultWeightedEdge e8 = graph.addEdge("vertex3", "vertex2");
+        graph.setEdgeWeight(e8, 2);
+
+        DefaultWeightedEdge e9 = graph.addEdge("vertex1", "vertex3");
+        graph.setEdgeWeight(e9, 10);
+
+        DefaultWeightedEdge e10 = graph.addEdge("vertex3", "vertex5");
+        graph.setEdgeWeight(e10, 1);
+
+        System.out.println(Graphs.neighborListOf(graph, "vertex1"));
+
+/*
+        System.out.println("Shortest path from vertex1 to vertex5:");
+        List shortest_path = Collections.singletonList(DijkstraShortestPath.findPathBetween(graph, "vertex1", "vertex5"));
+        System.out.println(shortest_path);
+*/
+
+    }
+
 }
